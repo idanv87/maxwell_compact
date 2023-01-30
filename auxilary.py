0431df1344a85623da92d221257acf2705c3ea7f
idanv87 <idanv@campus.technion.ac.il>
2023-01-30 12:38:40 +0200

    last

--- auxilary.py
+++ auxilary.py
@@ -1,14 +1,19 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
+import pickle
+import multiprocessing
+import timeit
+import datetime
+from scipy import sparse
 
 import scipy
-from scipy.sparse import csr_matrix
+from scipy.sparse import csr_matrix, kron, identity
 from scipy.linalg import circulant
 from scipy.sparse import block_diag
 from scipy.sparse import vstack
 
-from scipy.sparse.linalg import cg
+from scipy.sparse.linalg import cg, spsolve
 import pylops
 import timeit
 
@@ -19,6 +24,7 @@ import matplotlib.pyplot as plt
 
 
 def create_A_2(n):
+
     kernely = np.zeros((n, 1))
     kernely[-1] = 1
     kernely[0] = 22
@@ -35,19 +41,24 @@ def create_A_2(n):
     A[-1, -3] = 4
     A[-1, -4] = -1
 
+
     return csr_matrix(A) / 24
 
 
-def E_step(dt, x, y, h, E, Hx, Hy, Dx, Dy,A):
-
+def E_step(dt, x, y, h, E, Hx, Hy, Dx, Dy, A):
     start = timeit.default_timer()
     assert E.shape[0] == E.shape[1]
 
     Uy = Hy[1:, 1:-1] - Hy[:-1, 1:-1]
     Ux = Hx[1:-1, 1:] - Hx[1:-1, :-1]
 
-    dHy_dx = scipy.sparse.linalg.inv(create_A_2(Uy.shape[0]))@(Uy / h)
-    dHx_dy = np.transpose((scipy.sparse.linalg.inv(create_A_2(Ux.shape[1]))@np.transpose(Ux) / h))
+    dHy_dx = spsolve(create_A_2(Uy.shape[0]), (Uy / h))
+
+    dHx_dy = np.transpose((spsolve(create_A_2(Ux.shape[1]), np.transpose(Ux) / h)))
+
+
+    # dHy_dx = scipy.sparse.linalg.inv(create_A_2(Uy.shape[0])) @ (Uy / h)
+    # dHx_dy = np.transpose((scipy.sparse.linalg.inv(create_A_2(Ux.shape[1])) @ np.transpose(Ux) / h))
 
     dHy_dx_minus_dHx_dy = np.zeros((E.shape[0], E.shape[1]))  # dHx/dy
     dHy_dx_minus_dHx_dy[1:-1, 1:-1] = dHy_dx - dHx_dy
@@ -62,18 +73,14 @@ def E_step(dt, x, y, h, E, Hx, Hy, Dx, Dy,A):
     # print('E_step Time: ', stop - start)
     # start = timeit.default_timer()
 
-    E_next = mod_helmholtz(x, y, BE.copy(), -F, -BF, k, Dx, Dy,A)
-    stop = timeit.default_timer()
+    E_next = mod_helmholtz(x, y, BE.copy(), -F, -BF, k, Dx, Dy, A)
     # print(stop-start)
     # print(q)
-
-
-
 
     return E + dt * E_next
 
 
-def Hy_step(dt, x, y, h, E, Hy, BHy, Dx, Dy, A):
+def Hy_step(dt, x, y, h, E, Hy, Dx, Dy, A):
     BHy = None
     assert E.shape[0] == E.shape[1]
 
@@ -81,9 +88,11 @@ def Hy_step(dt, x, y, h, E, Hy, BHy, Dx, Dy, A):
 
     Ex = E[1:, :] - E[:-1, :]
 
-    dE_dx = (scipy.sparse.linalg.inv(create_A_2(Hy.shape[0]))@ Ex / h)
+    dE_dx = spsolve(create_A_2(Hy.shape[0]) ,Ex / h)
 
-    # dE_dx= (E[1:, :] - E[:-1, :])/h
+    # dE_dx = (scipy.sparse.linalg.inv(create_A_2(Hy.shape[0])) @ Ex / h)
+
+
 
     k = (24 / (dt ** 2))
     BF = k * dE_dx
@@ -106,13 +115,16 @@ def Hy_step(dt, x, y, h, E, Hy, BHy, Dx, Dy, A):
     return Hy + dt * Hy_next
 
 
-def Hx_step(dt, x, y, h, E, Hx, BHx, Dx, Dy, A):
+def Hx_step(dt, x, y, h, E, Hx, Dx, Dy, A):
     BHx = None
     assert E.shape[0] == E.shape[1]
     # A = np.linalg.inv(create_A_2(Hx.shape[1]))
 
     Ey = E[:, 1:] - E[:, :-1]
-    dE_dy = np.transpose((scipy.sparse.linalg.inv(create_A_2(Hx.shape[1]))@np.transpose(Ey) / h))
+    dE_dy = np.transpose(spsolve(create_A_2(Hx.shape[1]),np.transpose(Ey) / h))
+
+
+    # dE_dy = np.transpose((scipy.sparse.linalg.inv(create_A_2(Hx.shape[1])) @ np.transpose(Ey) / h))
 
     k = 24 / (dt ** 2)
     BF = -k * dE_dy
@@ -138,11 +150,9 @@ def blocks(A, n):
     B = A
     for j in range(n - 1):
         # B = block_diag(B, A)
-        B=block_diag((B,A))
+        B = block_diag((B, A))
 
     return B
-
-
 
 
 def create_B(x, y, B):
@@ -159,7 +169,7 @@ def create_B(x, y, B):
     return Bx, By
 
 
-def mod_helmholtz(x, y, B, F, BF, k, Dx, Dy,C):
+def mod_helmholtz(x, y, B, F, BF, k, Dx, Dy, C):
     start = timeit.default_timer()
 
     DxF = Dx
@@ -175,19 +185,22 @@ def mod_helmholtz(x, y, B, F, BF, k, Dx, Dy,C):
     '''
     y''-ky=0
     '''
+
+
     Bx, By = create_B(x, y, B.copy())
     BxF, ByF = create_B(x, y, BF.copy())
 
-
     la = -k * (1 + k * (h ** 2) / 12)
     # start = timeit.default_timer()
-    A = C + la * scipy.sparse.identity(Dx.shape[0])
-    stop = timeit.default_timer()
+    A = C + la * identity(Dx.shape[0])
+
     v = (1 + k * (h ** 2) / 12) * F \
-        + ((h ** 2) / 12) * (Dx@ F + BxF +DyF@F + ByF) \
-        - Bx - By - (h ** 2 / 6) * (Dx@By + Bx)
+        + ((h ** 2) / 12) * (Dx @ F + BxF + DyF @ F + ByF) \
+        - Bx - By - (h ** 2 / 6) * (Dx @ By + Bx)
+
 
     x, exit_code = cg(A, v, tol=1e-09)
+
 
     B1 = B.copy()
     B1[1:-1, 1:-1] = x.reshape((B.shape[0] - 2, B.shape[1] - 2))
@@ -195,59 +208,118 @@ def mod_helmholtz(x, y, B, F, BF, k, Dx, Dy,C):
     # print('helm'+ str(stop-start))
     # print(q)
 
-
-
     return B1
 
 
 def create_Ds(x, y):
+    pass
+    # start = timeit.default_timer()
+    # dx = x[1] - x[0]
+    # dy = y[1] - y[0]
+    # Nx = len(x) - 2
+    # Ny = len(y) - 2
+    #
+    # N = Nx * Ny
+    #
+    # kernely = np.zeros((Ny, 1))
+    # kernely[-1] = 1
+    # kernely[0] = -2
+    # kernely[1] = 1
+    #
+    # Dy = circulant(kernely)
+    # Dy[-1, 0] = 0
+    # Dy[0, -1] = 0
+    # Dy = blocks(csr_matrix(Dy), Nx) / (dy ** 2)
+    #
+    # kernelx1 = np.zeros((Nx * Ny, 1))
+    # kernelx1[0] = -2
+    # kernelx1[-Ny] = 1
+    # Dx1 = csr_matrix(circulant(kernelx1)[0:Ny])
+    #
+    # kernelx2 = np.zeros((Nx * Ny, 1))
+    # kernelx2[Ny] = 1
+    # kernelx2[0] = -2
+    # Dx2 = csr_matrix(circulant(kernelx2)[-Ny:])
+    #
+    # kernelx3 = np.zeros((Nx * Ny, 1))
+    # kernelx3[0] = 1
+    # kernelx3[Ny] = -2
+    # kernelx3[2 * Ny] = 1
+    # Dx3 = csr_matrix(circulant(kernelx3)[2 * Ny:])
+    # # Dx = np.concatenate([Dx1, Dx3, Dx2]) / (dx ** 2)
+    # Dx = vstack([Dx1, Dx3, Dx2]) / (dx ** 2)
 
-    start = timeit.default_timer()
+
+
+
+# def create_fd(ns, path):
+#     for i, n in enumerate(ns):
+#         x = np.linspace(0, 1, n + 1)
+#         y = np.linspace(0, 1, n + 1)
+#         DxE, DyE = create_Ds(x, y)
+#         DxHx, DyHx = create_Ds(x, y[1:])
+#         DxHy, DyHy = create_Ds(x[1:], y)
+#         pickle.dump([DxE,DyE,DxHx,DyHx,DxHy,DyHy], open(path + 'fd_matrices/' + str(n) + '.pkl', "wb"))
+
+def create_D2(x):
+    Nx=len(x[1:-1])
     dx = x[1] - x[0]
-    dy = y[1] - y[0]
-    Nx = len(x) - 2
-    Ny = len(y) - 2
-
-    N = Nx * Ny
-
-    kernely = np.zeros((Ny, 1))
-    kernely[-1] = 1
-    kernely[0] = -2
-    kernely[1] = 1
-
-    Dy = circulant(kernely)
+    kernel = np.zeros((Nx, 1))
+    kernel[-1] = 1
+    kernel[0] = -2
+    kernel[1] = 1
+    D2 = circulant(kernel)
+    D2[0,-1]=0
+    D2[-1,0]=0
+    D2=csr_matrix(D2)
+    return D2/dx/dx
 
 
-
-    Dy[-1, 0] = 0
-    Dy[0, -1] = 0
-    Dy = blocks(csr_matrix(Dy), Nx) / (dy ** 2)
+def create_Ds2(x,y):
+    return kron(create_D2(x), identity(len(y)-2) ), kron( identity(len(x)-2),create_D2(y))
 
 
-
-
-    kernelx1 = np.zeros((Nx * Ny, 1))
-    kernelx1[0] = -2
-    kernelx1[-Ny] = 1
-    Dx1 = csr_matrix(circulant(kernelx1)[0:Ny])
-
-    kernelx2 = np.zeros((Nx * Ny, 1))
-    kernelx2[Ny] = 1
-    kernelx2[0] = -2
-    Dx2 = csr_matrix(circulant(kernelx2)[-Ny:])
-
-
-    kernelx3 = np.zeros((Nx * Ny, 1))
-    kernelx3[0] = 1
-    kernelx3[Ny] = -2
-    kernelx3[2 * Ny] = 1
-    Dx3 = csr_matrix(circulant(kernelx3)[2 * Ny:])
-    # Dx = np.concatenate([Dx1, Dx3, Dx2]) / (dx ** 2)
-    Dx = vstack([Dx1, Dx3, Dx2]) / (dx ** 2)
+def create_D1(x):
+    Nx = len(x)
+    dx = x[1] - x[0]
+    kernel = np.zeros((Nx, 1))
+    kernel[-1] = 1
+    kernel[0] = -1
+    Dx = circulant(kernel)
+    Dx[0,-1]=0
+    Dx[-1,0]=0
+    return csr_matrix(Dx[:-1,:]/dx)
+def f_E(omega,kx,ky,x,y,t):
+    X,Y=np.meshgrid( x, y, indexing='ij')
+    return  np.cos(omega * t) * np.sin(math.pi * kx * X) * np.sin(math.pi * ky * Y)
+    # BF=-(omega**2+2)*E_a[0]
+    # F=BF[1:-1,1:-1].flatten()
+def f_Hx(omega, kx, ky, x, y, t, dt, h):
+    X, Y = np.meshgrid(x, y, indexing='ij')
+    return  -(np.sin(omega * (t + dt / 2)) * np.sin(math.pi * kx * X) * np.cos(
+        math.pi * ky * (Y + h / 2)) * math.pi * ky / omega)[:,:-1]
+def f_Hy(omega, kx, ky, x, y, t, dt, h):
+    X, Y = np.meshgrid(x, y, indexing='ij')
+    return  (np.sin(omega * (t + dt / 2)) * np.cos(math.pi * kx * (X + h / 2)) * np.sin(
+        math.pi * ky * Y) * math.pi * kx / omega)[:-1,:]
 
 
 
 
 
 
-    return csr_matrix(Dx), csr_matrix(Dy)
+# def create_circulant():
+#   N = 7
+#
+#   vals = np.array([2.0 / 3, -1.0 / 12, 1.0 / 12, -2.0 / 3])
+#
+#   offsets = np.array([1, 2, N - 2, N - 1])
+#
+#   dupvals = np.concatenate((vals, vals[::-1]))
+#
+#   dupoffsets = np.concatenate((offsets, -offsets))
+#
+#   a = sparse.diags(dupvals, dupoffsets, shape=(N, N))
+#   return a
+
+
--- schemes.py
+++ schemes.py
@@ -2,13 +2,14 @@ import matplotlib.pyplot as plt
 import numpy as np
 import math
 import matplotlib.patches as mpatches
-from auxilary import Hy_step, E_step, mod_helmholtz, Hx_step, create_Ds
+from auxilary import Hy_step, E_step, mod_helmholtz, Hx_step, create_Ds2,f_Hy,f_E,f_Hx
 import multiprocessing as mp
 from joblib import Parallel, delayed
 from concurrent.futures import ThreadPoolExecutor
 import ray
 from scipy.sparse import csc_matrix
 import timeit
+import pickle
 
 
 # ray.init()
@@ -21,86 +22,74 @@ import timeit
 #      return Hy_step(dt, x, y, h, E0, Hy0, BHy)
 
 
-def max_solver(dt, x, y, h, time_steps, E_a, Hx_a, Hy_a, DxE, DyE, DxHx, DyHx, DxHy, DyHy, AE, AHx, AHy):
-    E_tot = []
-    Hx_tot = []
-    Hy_tot = []
-    E0 = E_a[0]
-    Hx0 = Hx_a[0]
-    Hy0 = Hy_a[0]
-    for t in range(time_steps - 1):
-        print(t)
-        E_tot.append(E0)
-        Hx_tot.append(Hx0)
-        Hy_tot.append(Hy0)
+def max_solver(dt, x, y, h, time_steps, DxE, DyE, DxHx, DyHx, DxHy, DyHy, AE, AHx, AHy):
+
+    errE=[]
+    errHx=[]
+    errHy=[]
+
+    E0 = f_E(omega, kx, ky, x, y, 0)
+    Hx0 = f_Hx(omega, kx, ky, x, y, 0, dt, h)
+    Hy0 = f_Hy(omega, kx, ky, x, y, 0, dt, h)
+    for i,t in enumerate(range(time_steps - 1)):
+        errE.append(np.mean(abs(E0[1:-1, 1:-1] - f_E(omega, kx, ky, x, y, i*dt)[1:-1, 1:-1])))
+        errHx.append(np.mean(abs(Hx0[1:-1, :] -f_Hx(omega, kx, ky, x, y, i*dt, dt, h)[1:-1, :])))
+        errHy.append(np.mean(abs(Hy0[:, 1:-1] - f_Hy(omega, kx, ky, x, y, i*dt, dt, h)[:, 1:-1])))
+
+
+
+        # errHx = np.array([np.mean(abs(Hx_tot[i][1:-1, :] - f_Hx(omega, kx, ky, x, y, i * dt, dt, h)[1:-1, :])) for i in
+        #                   range(len(Hx_tot))])
+        # errHy = np.array([np.mean(abs(Hy_tot[i][:, 1:-1] - f_Hy(omega, kx, ky, x, y, i * dt, dt, h)[:, 1:-1])) for i in
+        #                   range(len(Hy_tot))])
+        #
+        # E_tot.append(E0)
+        # Hx_tot.append(Hx0)
+        # Hy_tot.append(Hy0)
         start = timeit.default_timer()
         E0 = E_step(dt, x, y, h, E0.copy(), Hx0.copy(), Hy0.copy(), DxE, DyE, AE)
+        Hx0 = Hx_step(dt, x, y, h, E0.copy(), Hx0.copy(), DxHx, DyHx, AHx)
+        Hy0 = Hy_step(dt, x, y, h, E0.copy(), Hy0.copy(), DxHy, DyHy, AHy)
 
-
-        # ret_id1 = fHx.remote(dt, x, y, h, E0.copy(), Hx0.copy(), (Hx_a[t+1]-Hx_a[t])/dt)
-        # ret_id2 = fHy.remote(dt, x, y, h, E0.copy(), Hy0.copy(), (Hy_a[t+1]-Hy_a[t])/dt)
-        # Hx0, Hy0 = ray.get([ret_id1, ret_id2])
-        Hx0 = Hx_step(dt, x, y, h, E0.copy(), Hx0.copy(), (Hx_a[t + 1] - Hx_a[t]) / dt, DxHx, DyHx, AHx)
-        Hy0 = Hy_step(dt, x, y, h, E0.copy(), Hy0.copy(), (Hy_a[t + 1] - Hy_a[t]) / dt, DxHy, DyHy, AHy)
-        stop = timeit.default_timer()
-
-    errE = np.array([np.mean(abs(E_tot[i][1:-1, 1:-1] - E_a[i][1:-1, 1:-1])) for i in range(len(E_tot))])
-    errHx = np.array([np.mean(abs(Hx_tot[i][1:-1, :] - Hx_a[i][1:-1, :])) for i in range(len(Hx_tot))])
-    errHy = np.array([np.mean(abs(Hy_tot[i][:, 1:-1] - Hy_a[i][:, 1:-1])) for i in range(len(Hy_tot))])
+    # errE = np.array([np.mean(abs(E_tot[i][1:-1, 1:-1] - E_a[i][1:-1, 1:-1])) for i in range(len(E_tot))])
+    # errE = np.array([np.mean(abs(E_tot[i][1:-1, 1:-1] - f_E(omega, kx, ky, x, y, i*dt)[1:-1, 1:-1])) for i in range(len(E_tot))])
+    # errHx = np.array([np.mean(abs(Hx_tot[i][1:-1, :] - f_Hx(omega, kx, ky, x, y, i*dt, dt, h)[1:-1, :])) for i in range(len(Hx_tot))])
+    # errHy = np.array([np.mean(abs(Hy_tot[i][:, 1:-1] - f_Hy(omega, kx, ky, x, y, i*dt, dt, h)[:, 1:-1])) for i in range(len(Hy_tot))])
+    errE=np.array(errE)
     return ((np.mean(errE) + np.mean(errHx) + np.mean(errHy)) / 3)
 
 
-kx = 4
-ky = 3
+kx = 2
+ky = 1
 omega = math.pi * np.sqrt(kx ** 2 + ky ** 2)
 
 err = []
-ns = [64,84, 128]
-# N=[21,41,81]
+ns = [16, 32, 64,128,256]
 
+path = '/Users/idanversano/Documents/visual studio/maxwell_compact/'
 for i, n in enumerate(ns):
     x = np.linspace(0, 1, n + 1)
     y = np.linspace(0, 1, n + 1)
     h = x[1] - x[0]
-    DxE, DyE = create_Ds(x, y)
-    DxHx, DyHx = create_Ds(x, y[1:])
-    DxHy, DyHy = create_Ds(x[1:], y)
-    start= timeit.default_timer()
-    AE = DxE + DyE + ((h ** 2) / 6) * DxE@DyE
-    # stop = timeit.default_timer()
-    # print(stop-start)
-    # print(q)
-    AHx = DxHx + DyHx + ((h ** 2) / 6) * DxHx@DyHx
-    AHy = DxHy + DyHy + ((h ** 2) / 6) * DxHy@DyHy
-    # AE=DxE + DyE + ((h ** 2) / 6) * np.matmul(DxE, DyE)
-    # AHx = DxHx + DyHx + ((h ** 2) / 6) * np.matmul(DxHx, DyHx)
-    #
-    # AHy = DxHy + DyHy + ((h ** 2) / 6) * np.matmul(DxHy, DyHy)
+    DxE, DyE = create_Ds2(x, y)
+    DxHx, DyHx = create_Ds2(x, y[1:])
+    DxHy, DyHy = create_Ds2(x[1:], y)
+    start = timeit.default_timer()
+    AE = DxE + DyE + ((h ** 2) / 6) * DxE @ DyE
 
-
-
-
-
-
+    AHx = DxHx + DyHx + ((h ** 2) / 6) * DxHx @ DyHx
+    AHy = DxHy + DyHy + ((h ** 2) / 6) * DxHy @ DyHy
 
     time_steps = 2 * ns[i] + 1
     T = 1
     dt = T / (time_steps - 1)
 
-    t, X, Y = np.meshgrid(np.linspace(0, T, time_steps), x, y, indexing='ij')
-    E_a = np.cos(omega * t) * np.sin(math.pi * kx * X) * np.sin(math.pi * ky * Y)
-    # BF=-(omega**2+2)*E_a[0]
-    # F=BF[1:-1,1:-1].flatten()
-    Hx_a = -(np.sin(omega * (t + dt / 2)) * np.sin(math.pi * kx * X) * np.cos(
-        math.pi * ky * (Y + h / 2)) * math.pi * ky / omega)[:, :, :-1]
-    Hy_a = (np.sin(omega * (t + dt / 2)) * np.cos(math.pi * kx * (X + h / 2)) * np.sin(
-        math.pi * ky * Y) * math.pi * kx / omega)[:, :-1, :]
+
     # err.append(np.mean(abs(Hy_step(dt, x, y, h, E_a[1], Hy_a[0])[:,1:-1]-Hy_a[1][:,1:-1])))
     # err.append(np.mean(abs(Hx_step(dt, x, y, h, E_a[1], Hx_a[0])[1:-1, :] - Hx_a[1][1:-1,:])))
-    err.append(max_solver(dt, x, y, h, time_steps, E_a, Hx_a, Hy_a, DxE, DyE,
+    err.append(max_solver(dt, x, y, h, time_steps, DxE, DyE,
                           DxHx, DyHx, DxHy, DyHy,
                           AE, AHx, AHy))
-
 
     # err.append(max_solver(dt, x, y, h, time_steps, E_a, Hx_a, Hy_a, DxE.toarray(), DyE.toarray(),
     #                       DxHx.toarray(), DyHx.toarray(), DxHy.toarray(), DyHy.toarray(),
@@ -125,3 +114,131 @@ print(np.diff(y) / np.diff(x))
 # print(np.diff(y) / np.diff(x))
 
 plt.show()
+# import matplotlib.pyplot as plt
+# import numpy as np
+# import math
+# import pickle
+# from tqdm import tqdm
+# import matplotlib.patches as mpatches
+# from auxilary import Hy_step, E_step, mod_helmholtz, Hx_step, create_Ds, create_fd, create_D2, create_D1, create_Ds2
+# from auxilary import Hx_a, Hy_a, E_a
+# import multiprocessing as mp
+# from joblib import Parallel, delayed
+# from concurrent.futures import ThreadPoolExecutor
+#
+# from scipy.sparse import csc_matrix
+# import timeit
+#
+#
+# # ray.init()
+# # @ray.remote
+# # def fHx(dt, x, y, h, E0, Hx0, BHx):
+# #      return Hx_step(dt, x, y, h, E0, Hx0, BHx)
+# #
+# # @ray.remote
+# # def fHy(dt, x, y, h, E0, Hy0, BHy):
+# #      return Hy_step(dt, x, y, h, E0, Hy0, BHy)
+#
+# path='/Users/idanversano/Documents/visual studio/maxwell_compact/'
+#
+# def max_solver(dt, x, y, h, time_steps, DxE, DyE, DxHx, DyHx, DxHy, DyHy, AE, AHx, AHy):
+#     E_tot = []
+#     Hx_tot = []
+#     Hy_tot = []
+#     E0 = E_a(omega,kx,ky,x,y,0)
+#     Hx0 = Hx_a(omega, kx, ky, x, y, 0, dt, h)
+#     Hy0 = Hy_a(omega, kx, ky, x, y, 0, dt, h)
+#     for i,t in enumerate(range(time_steps - 1)):
+#         E_tot.append(np.mean(abs(E0[1:-1, 1:-1] - E_a(omega, kx, ky, x, y, t)[1:-1, 1:-1])))
+#         Hx_tot.append(np.mean(abs(Hx0[1:-1, :] -Hx_a(omega, kx, ky, x, y, t, dt, h)[1:-1, :])))
+#         Hy_tot.append(np.mean(abs(Hy0[:, 1:-1] - Hy_a(omega, kx, ky, x, y, t, dt, h)[:, 1:-1])))
+#
+#         E0 = E_step(dt, x, y, h, E0.copy(), Hx0.copy(), Hy0.copy(), DxE, DyE, AE)
+#         Hx0 = Hx_step(dt, x, y, h, E0.copy(), Hx0.copy(), DxHx, DyHx, AHx)
+#         Hy0 = Hy_step(dt, x, y, h, E0.copy(), Hy0.copy(), DxHy, DyHy, AHy)
+#
+#
+#     return ((np.mean(E_tot) + np.mean(Hx_tot) + np.mean(Hy_tot)) / 3)
+#
+#
+# kx = 1
+# ky = 2
+# omega = math.pi * np.sqrt(kx ** 2 + ky ** 2)
+#
+# err = []
+#
+# ns = [16,32,64]
+# # create_fd(ns,path)
+# # print(q)
+# # N=[21,41,81]
+#
+# for i, n in tqdm(enumerate(ns)):
+#     x = np.linspace(0, 1, n + 1)
+#     y = np.linspace(0, 1, n + 1)
+#     h = x[1] - x[0]
+#     # with open(path + 'fd_matrices/' + str(n) + '.pkl', 'rb') as file:
+#     #      DxE, DyE, DxHx, DyHx, DxHy, DyHy = pickle.load(file)
+#
+#     DxE, DyE = create_Ds2(x, y)
+#     DxHx, DyHx = create_Ds2(x, y[1:])
+#     DxHy, DyHy = create_Ds2(x[1:], y)
+#
+#     AE = DxE + DyE + ((h ** 2) / 6) * DxE@DyE
+#     AHx = DxHx + DyHx + ((h ** 2) / 6) * DxHx@DyHx
+#     AHy = DxHy + DyHy + ((h ** 2) / 6) * DxHy@DyHy
+#
+#
+#     # AE=DxE + DyE + ((h ** 2) / 6) * np.matmul(DxE, DyE)
+#     # AHx = DxHx + DyHx + ((h ** 2) / 6) * np.matmul(DxHx, DyHx)
+#     #
+#     # AHy = DxHy + DyHy + ((h ** 2) / 6) * np.matmul(DxHy, DyHy)
+#
+#
+#
+#
+#
+#
+#
+#     time_steps = 2 * ns[i] + 1
+#     T = 1
+#     dt = T / (time_steps - 1)
+#     # E_a = np.cos(omega * t) * np.sin(math.pi * kx * X) * np.sin(math.pi * ky * Y)
+#     # # BF=-(omega**2+2)*E_a[0]
+#     # # F=BF[1:-1,1:-1].flatten()
+#     # Hx_a = -(np.sin(omega * (t + dt / 2)) * np.sin(math.pi * kx * X) * np.cos(
+#     #     math.pi * ky * (Y + h / 2)) * math.pi * ky / omega)[:, :, :-1]
+#     # Hy_a = (np.sin(omega * (t + dt / 2)) * np.cos(math.pi * kx * (X + h / 2)) * np.sin(
+#     #     math.pi * ky * Y) * math.pi * kx / omega)[:, :-1, :]
+#     print(n)
+#     # err.append(np.mean(abs(Hy_step(dt, x, y, h, E_a[1], Hy_a[0])[:,1:-1]-Hy_a[1][:,1:-1])))
+#     # err.append(np.mean(abs(Hx_step(dt, x, y, h, E_a[1], Hx_a[0])[1:-1, :] - Hx_a[1][1:-1,:])))
+#
+#     err.append(max_solver(dt, x, y, h, time_steps, DxE, DyE,
+#                           DxHx, DyHx, DxHy, DyHy,
+#                           AE, AHx, AHy))
+#
+#
+#     # err.append(max_solver(dt, x, y, h, time_steps, E_a, Hx_a, Hy_a, DxE.toarray(), DyE.toarray(),
+#     #                       DxHx.toarray(), DyHx.toarray(), DxHy.toarray(), DyHy.toarray(),
+#     #                       AE.toarray(), AHx.toarray(), AHy.toarray()))
+#     # err.append(np.mean(abs(E_step(dt, x, y, h, E_a[0], Hx_a[0], Hy_a[0]) - E_a[1])))
+#     # err.append(np.mean(abs(mod_helmholtz(x, y,BF*0,F,BF ,2) - E_a[0])))
+#
+# #
+# x = np.log(1 / np.array(ns))
+# y = np.log(np.array(err))
+# plt.plot(ns, err)
+# print(err)
+# print(np.diff(y) / np.diff(x))
+# #
+# # x=np.log(1/np.array(nT))
+# # y=np.log(np.array(err))
+# # plt.plot(nT,err)
+# # print(np.diff(y) / np.diff(x))
+#
+# # x=np.log(np.array(Ts))
+# # y=np.log(np.array(err))
+# # plt.plot(1/np.array(Ts),err)
+# # print(np.diff(y) / np.diff(x))
+#
+# plt.show()
